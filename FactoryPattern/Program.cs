98d2cb6117a8a156469d05c08d0a80cb388fad83
TonyDolenti <antoniobdolenti@gmail.com>
2023-03-07 22:41:38 -0500

    initial commit

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool input = false;
+            int wheelCount;
+            do
+            {
+                Console.WriteLine("Enter the amount of wheels you want your vehicle to have, either 2 or 4!");
+
+                
+
+                input = int.TryParse(Console.ReadLine(), out wheelCount);
+            } while (input == false);
+
+            
+            var vehicle = VehicleFactory.GetVehicle(wheelCount);
+            vehicle.Drive();
         }
     }
 }
